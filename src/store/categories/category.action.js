e363782acef91047c59b4ebab64171047d776ce2
Sarath chowdary <cheekatisarath@gmail.com>
2023-04-09 11:27:05 +0530

    complete

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,28 +1,15 @@
-import { CATEGORIES_ACTION_TYPES } from "./category.types";
-import { createAction } from "../../utils/reducer/reducer.utils";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { CATEGORIES_ACTION_TYPES } from './category.types';
 
-export const fetchCategoriesStart = () => {
+import { createAction } from '../../utils/reducer/reducer.utils';
+
+export const fetchCategoriesStart = () =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
-};
 
-export const fetchCategoriesSuccess = (categoriesArray) => {
+export const fetchCategoriesSuccess = (categoriesArray) =>
   createAction(
     CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
     categoriesArray
   );
-};
 
-export const fetchCategoriesFailed = (error) => {
+export const fetchCategoriesFailed = (error) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-};
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
-  try {
-    const categoriesArray = await getCategoriesAndDocuments("categories");
-    dispatch(fetchCategoriesSuccess(categoriesArray));
-  } catch (error) {
-    dispatch(fetchCategoriesFailed(error));
-  }
-};
